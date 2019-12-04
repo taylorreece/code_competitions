6e2e349743a5612357755a4f9ccde476a4e57a00
Taylor Reece <taylor.reece@gmail.com>
2019-12-04 10:45:55 -0600

    Day 4 solution

---
+++ advent-of-code/2019/04/part1/solution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+with open('input.txt', 'r') as infile:
+    my_min, my_max = [int(x) for x in infile.read().strip().split('-')]
+
+count = 0
+for i in range(10):
+    for j in range(i, 10):
+        for k in range(j, 10):
+            for l in range(k, 10):
+                for m in range(l, 10):
+                    for n in range(m, 10):
+                        if i == j or j == k or k == l or l == m or m == n:
+                            my_num = i*100000 + j * 10000 + k * 1000 + l * 100 + m * 10 + n
+                            if my_min <= my_num and my_num <= my_max:
+                                count += 1
+print(f'Count: {count}')
